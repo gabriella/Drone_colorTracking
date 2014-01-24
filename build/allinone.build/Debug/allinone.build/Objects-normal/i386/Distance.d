f5547010ba41f2f65be6bfa5e1b994cc1b0c1362
markkleeb <kleeb930@gmail.com>
2014-01-23 21:05:37 -0500

    remove build

--- build/allinone.build/Debug/allinone.build/Objects-normal/i386/Distance.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Kleeb3/Documents/OpenFrameWorks/of_0080b/apps/myApps/Drone_colorTracking/../../../addons/ofxCv/libs/ofxCv/src/Distance.cpp \
-  ../../../addons/ofxCv/libs/ofxCv/include/ofxCv/Distance.h
--- build/allinone.build/Debug/allinone.build/Objects-normal/i386/macGlutFix.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Kleeb3/Documents/OpenFrameWorks/of_0080b/apps/myApps/Drone_colorTracking/src/macGlutFix.m \
-  /Users/Kleeb3/Documents/OpenFrameWorks/of_0080b/apps/myApps/Drone_colorTracking/src/macGlutfix.h
